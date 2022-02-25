19e923e19e0e2bfbd8cf97fb86f072219b75d683
Edisson Palomares <palomaredif@gmail.com>
2022-02-25 16:06:08 -0500

    hook local storage

--- src/App.js
+++ src/App.js
@@ -2,26 +2,62 @@
 import React from 'react';
 import {AppUI} from './components/AppUI';
 
-const default_todos = [
+// const default_todos = [
+//     {
+//         name:"cut onions",
+//         completed:false
+//     },
+//     {
+//         name:"watch react course",
+//         completed:false
+//     },
+//     {
+//         name:"play guitar",
+//         completed:false
+//     },
+// ];
+
+// localStorage.setItem('TODOS_V1',JSON.stringify(default_todos));
+
+function useLocalStorage(item_name,initial_value)
+{
+    const local_storage_item = localStorage.getItem(item_name);
+
+    let parsed_item;
+
+    if(!local_storage_item)
     {
-        name:"cut onions",
-        completed:false
-    },
+        localStorage.setItem(item_name,JSON.stringify(initial_value));
+        parsed_item = initial_value;
+    }
+    else
     {
-        name:"watch react course",
-        completed:false
-    },
+        parsed_item = JSON.parse(local_storage_item);
+    }
+
+    const [item,set_item] = React.useState(parsed_item);
+
+
+    const save_item = (todos)=>
     {
-        name:"play guitar",
-        completed:false
-    },
-];
+        const string_todos = JSON.stringify(todos);
+        localStorage.setItem(item_name,string_todos);        
+        set_item(todos);
+    }
+
+    return [
+        item,
+        save_item
+    ]
+}
 
 function App(props) {
-    const [todos,set_todos] = React.useState(default_todos);
+
+    const [todos,save_todos] = useLocalStorage('TODOS_V1',[]);
     const [search_value, set_search_value] = React.useState('');
 
     let searched_todos = [];
+
 
     const completed_todos = ()=>{
         let completed = todos.filter((todo)=>{
@@ -49,14 +85,14 @@ function App(props) {
         const new_todos = [...todos];
         new_todos[todo_index].completed = true;
 
-        set_todos(new_todos);
+        save_todos(new_todos);
     };
 
     const delete_todo = (name)=>{
         const todo_index = todos.findIndex(todo=>todo.name == name);
         const new_todos = [...todos];
         new_todos.splice(todo_index, 1);
-        set_todos(new_todos);
+        save_todos(new_todos);
     }
 
     if(!(search_value.length >= 1))
